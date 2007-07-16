07bb58f387a3e9d0762922d0dcbbe2b27f9b2543
michael.tokar <michael.tokar@b0ad8c8f-171f-0410-9f2b-89caf2f736b1>
2007-07-16 12:59:11 +0000

    VERSION 0.15 For list of changes, see Changelog on googlecode site.

---  logscrobbler/MyLastTen.cs
+++  logscrobbler/MyLastTen.cs
@@ -22,133 +22,54 @@ namespace LogScrobbler
 	/// </summary>
 	public partial class MyLastTen
 	{
-		System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-		public string myuser;
+		System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+		public string userName;
 		public string swColor;
-		public string myDir;
 		
-		public MyLastTen(string dir)
+		public MyLastTen(string userName)
 		{
-			
 			//
 			// The InitializeComponent() call is required for Windows Forms designer support.
 			//
 			InitializeComponent();
-			myDir = dir;
-			GatherUser();
-			getMyLastTen();
-			//
-			// TODO: Add constructor code after the InitializeComponent() call.
-			//
+
+			this.userName = userName;
+			lstLastTenTracks.BackgroundImage = (System.Drawing.Bitmap)resources.GetObject("$this.BackgroundImage");
 		}
 		
-		public void GatherUser(){
-			try{
-				StreamReader sett = new StreamReader(myDir + "\\LogScrobbler.txt");
-				string setting;
-				string[] fields;
-				int count = 0;
-				while ((setting = sett.ReadLine()) != null)
-				{
-					fields = setting.Split('=');
-					if(count == 0) {
-						myuser = fields[1];
-					}
-					if(count == 5) {
-						swColor = fields[1];
-					}
-					count++;
-				}
-				sett.Close();
-			}
-			catch
-			{
-				
-			}
-			if(swColor == "sw2") {
-				listView1.BackgroundImage =
-					(System.Drawing.Bitmap)resources.GetObject("$this.bg2");
-			} else {
-				listView1.BackgroundImage =
-					(System.Drawing.Bitmap)resources.GetObject("$this.BackgroundImage");
-			}
-		}
-		
-		public void getMyLastTen(){
-			try
-			{
-				//http://ws.audioscrobbler.com/1.0/user/kernelsandirs/recenttracks.rss
-				System.Net.WebClient Client = new WebClient();
-				//Client.Proxy = new WebProxy();
-				Stream strm = Client.OpenRead("http://ws.audioscrobbler.com/1.0/user/" + myuser + "/recenttracks.rss");
-				StreamReader sr = new StreamReader(strm);
-				string line;
-				string lastEntry = "";
-				string oktoGo = "";
-				string title = "";
-				string link = "";
-				do
-				{
-					line = sr.ReadLine();
-					Regex regex1 = new Regex("<item>");
-					if(regex1.IsMatch(line)) {
-						oktoGo = "OK";
-					}
-					
-					Regex regex2= new Regex("<title>");
-					if(regex2.IsMatch(line)) {
-						if (oktoGo == "OK"){
-							title = line.Replace("<title>","");
-							title = title.Replace("</title>","");
-							title = title.Trim();
-						}
-					}
-					Regex regex3= new Regex("<link>");
-					if(regex3.IsMatch(line)) {
-						if (oktoGo == "OK"){
-							link = line.Replace("<link>","");
-							link = link.Replace("</link>","");
-							link = link.Trim();
-						}
-					}
-					
-					Regex regex = new Regex("<pubDate>");
-					if(regex.IsMatch(line)) {
-						if (oktoGo == "OK"){
-							lastEntry = line.Replace("<pubDate>","");
-							lastEntry = lastEntry.Replace("</pubDate>","");
-							lastEntry = lastEntry.Replace(lastEntry.Substring(lastEntry.Length-5,5),"");
-							lastEntry = lastEntry.Trim();
-							ListViewItem item = listView1.Items.Add(title);
-							item.SubItems.Add(lastEntry);
-							item.Tag = link;
-						}
-					}
-				}
-				while (line !=null);
-				strm.Close();
-			}
-			catch
-			{
-				
-			}
-		}
-		
-		void ListView1DoubleClick(object sender, System.EventArgs e)
+		public void getMyLastTen()
 		{
-			ProcessStartInfo sInfo = new ProcessStartInfo(listView1.SelectedItems[0].Tag.ToString());
-			Process.Start(sInfo);
+			TrackList lastTenTracks = AudioscrobblerWebServices.GetRecentTracks(userName);
+
+			foreach (Track currentTrack in lastTenTracks)
+			{
+				string label = string.Format("{0} - {1}", currentTrack.ArtistName, currentTrack.TrackName);
+				ListViewItem item = lstLastTenTracks.Items.Add(label);
+				item.SubItems.Add(currentTrack.TrackPlayedAsString);
+			}
 		}
 		
-		void Button1Click(object sender, System.EventArgs e)
+		void lstLastTenTracks_DoubleClick(object sender, System.EventArgs e)
 		{
-			listView1.Items.Clear();
+			//ProcessStartInfo sInfo = new ProcessStartInfo(listView1.SelectedItems[0].Tag.ToString());
+			//Process.Start(sInfo);
+		}
+		
+		void btnReload_Click(object sender, System.EventArgs e)
+		{
+			lstLastTenTracks.Items.Clear();
 			getMyLastTen();
 		}
 		
-		void Button2Click(object sender, System.EventArgs e)
+		void btnClose_Click(object sender, System.EventArgs e)
 		{
 			Close();
+		}
+
+		private void MyLastTen_Load(object sender, EventArgs e)
+		{
+			lstLastTenTracks.Items.Clear();
+			getMyLastTen();
 		}
 	}
 }
